cbe9e9b052177a59c2de7ee488e67a372a7e4e5b
Gil Gonen <ggonen@cisco.com>
2018-08-29 10:00:42 +0300

    Upload message image

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ var userName = "Guest"; // The current user name
 var userIconUrl = "https://upload.wikimedia.org/wikipedia/commons/d/d3/User_Circle.png"; // the current user icon url
 
 function displayMessages() {
-    var messagesHtml = buildMessagesHtml();
-	divChatBox.innerHTML = messagesHtml; // insert chat divs into the #divChatBox div			
+    divChatBox.innerHTML = buildMessagesHtml();
 }
 
 function buildMessagesHtml() {
     var messagesHtml = "";
 
-    for (var i=0; i < messagesArray.length; i++) {
+    for (var i = messagesArray.length - 1; i >= 0; i--) {
         messagesHtml += buildMessageHtml(messagesArray[i]);
     }
 
@@ -20,12 +19,20 @@ function buildMessagesHtml() {
 }
 
 function buildMessageHtml(msg) {
-    var messageHtml = "<div class='msgln'><b>" + msg.userName + "</b>: " + msg.messageText + "<br /></div>";
+    var messageHtml = "<div class='msgln'>";
+    messageHtml += "<b>" + msg.userName + ":</b>";
+    if (msg.imageUrl) {
+        messageHtml += "<br /><img src='" + msg.imageUrl + "'><br />";
+    }
+    if (msg.messageText) {
+        messageHtml += msg.messageText;
+    }
+    messageHtml += "</div>";
     return messageHtml;
 }
 
 function insertMessageHtml(msg) {
-    divChatBox.innerHTML += buildMessageHtml(msg);
+    divChatBox.innerHTML = buildMessageHtml(msg) + divChatBox.innerHTML;
 }
 
 function updateCurrentUser() {
@@ -67,21 +74,25 @@ function onLogoutClicked() {
 
 function onSubmitMessageClicked() {
     var messageText = textUserMsg.value.trim();
-        
-    if (!messageText) {
-        return false; // do not send empty messages
+    var newMessage = { userName: userName, messageText: messageText };
+
+    var imageFile;
+    if (inputMediaCapture.files && inputMediaCapture.files.length) {
+        imageFile = inputMediaCapture.files[0];
     }
 
-    var newMessage = { userName: userName, messageText: messageText };
-    sendNewMessage(newMessage);		
+    sendNewMessage(newMessage, imageFile, function() {
+        textUserMsg.value = "";
+        inputMediaCapture.value = null;
+    });
 
-    textUserMsg.value = "";
     return false;
 }
 
 // Shortcuts to DOM Elements.
 var buttonSubmitMsg = document.getElementById("buttonSubmitMsg");
 var textUserMsg = document.getElementById("textUserMsg");
+var inputMediaCapture = document.getElementById("inputMediaCapture");
 var pLogin = document.getElementById("pLogin");
 var pLogout = document.getElementById("pLogout");
 var buttonLogin = document.getElementById("buttonLogin");
@@ -104,7 +115,7 @@ var config = {
     authDomain: "pitputit-6af13.firebaseapp.com",
     databaseURL: "https://pitputit-6af13.firebaseio.com",
     projectId: "pitputit-6af13",
-    storageBucket: "",
+    storageBucket: "pitputit-6af13.appspot.com",
     messagingSenderId: "849608190982"
 };
 firebase.initializeApp(config);
@@ -124,12 +135,33 @@ var loadMessages = function() {
     })
 };
   
-function sendNewMessage(newMessage) {
+function sendNewMessage(newMessage, imageFile, onFinished) {
+    if (imageFile) {
+        var storageRef = firebase.storage().ref();
+        var imageRef = storageRef.child('messageImages/' + imageFile.name);
+        // TBD what if an image already exists with that name? => use also imageFile.lastModified
+        
+        imageRef.put(imageFile).then(function onComplete(snapshot) {
+            console.log("sendNewMessage: image upload done");
+            snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                console.log("sendNewMessage: image downloadURL = " + downloadURL);
+                newMessage.imageUrl = downloadURL;
+                sendNewMessage(newMessage, null, onFinished);
+            });
+        }).catch(function(err) {
+            alert("sendNewMessage: image upload failed: " + err);
+            onFinished(false);
+        });
+        return;
+    }
+
     messagesRef.push(newMessage, function onComplete(err) {
         if (err) {
-            console.error("sendNewMessage: push failed with " + err);
+            alert("sendNewMessage: push failed with " + err);
+            onFinished(false);
         } else {
             console.log("sendNewMessage: done");
+            onFinished(true);
         }
     });
 }
