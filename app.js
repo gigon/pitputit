38a47b7790f1206c6d57c9fc6093c79a3e2d65fb
gigon <gil.gonen@gmail.com>
2018-08-30 19:35:06 +0300

    save remote image url

--- app.js
+++ app.js
@@ -4,42 +4,35 @@ var messagesArray = [];  // The messages
 var userName = "Guest"; // The current user name
 var userIconUrl = "https://upload.wikimedia.org/wikipedia/commons/d/d3/User_Circle.png"; // the current user icon url
 
-function displayMessages() {
-    var messagesHtml = buildMessagesHtml();
-	divChatBox.innerHTML = messagesHtml; // insert chat divs into the #divChatBox div			
-}
-
-function buildMessagesHtml() {
-    var messagesHtml = "";
-
-    for (var i=0; i < messagesArray.length; i++) {
-        messagesHtml += buildMessageHtml(messagesArray[i]);
-    }
-
-    return messagesHtml;
-}
-
 function buildMessageHtml(msg) {
-    var messageHtml = "<div class='msgln'><b>" + msg.userName + "</b>: " + msg.messageText + "<br /></div>";
+    var messageHtml = "<div class='msgln'>";
+    messageHtml += "<b>" + msg.userName + ":</b>";
+    if (msg.imageUrl) {
+        messageHtml += "<br /><img src='" + msg.imageUrl + "'><br />";
+    }
+    if (msg.messageText) {
+        messageHtml += msg.messageText;
+    }
+    messageHtml += "</div>";
     return messageHtml;
 }
 
 function insertMessageHtml(msg) {
-    divChatBox.innerHTML += buildMessageHtml(msg);
+    divChatBox.innerHTML = buildMessageHtml(msg) + divChatBox.innerHTML;
 }
 
 function updateCurrentUser() {
     spanUserName.innerHTML = userName;
     userIcon.src = userIconUrl;
 
-    if (isUserSignedIn() == false) {
-        pLogin.style.display = "block";
-        pLogout.style.display = "none";
-        userIcon.style.display = "none";
-    } else {
+    if (isUserSignedIn()) {
         userIcon.style.display = "block";
         pLogout.style.display = "block";
         pLogin.style.display = "none";    
+    } else {
+        pLogin.style.display = "block";
+        pLogout.style.display = "none";
+        userIcon.style.display = "none";
     }
 }
 
@@ -67,21 +60,29 @@ function onLogoutClicked() {
 
 function onSubmitMessageClicked() {
     var messageText = textUserMsg.value.trim();
-        
-    if (!messageText) {
-        return false; // do not send empty messages
+    var imageUrl = textImageUrl.value.trim();
+
+    if (!messageText && !imageUrl) {
+        alert("Please select an image and/or fill text");
+        textUserMsg.focus();
+        return false;
     }
 
-    var newMessage = { userName: userName, messageText: messageText };
-    sendNewMessage(newMessage);		
+    var newMessage = { userName: userName, messageText: messageText, imageUrl: imageUrl };
+    sendNewMessage(newMessage, cleanNewMessageInputs);
 
-    textUserMsg.value = "";
     return false;
+}
+
+function cleanNewMessageInputs() {
+    textUserMsg.value = "";
+    textImageUrl.value = "";
 }
 
 // Shortcuts to DOM Elements.
 var buttonSubmitMsg = document.getElementById("buttonSubmitMsg");
 var textUserMsg = document.getElementById("textUserMsg");
+var textImageUrl = document.getElementById("textImageUrl");
 var pLogin = document.getElementById("pLogin");
 var pLogout = document.getElementById("pLogout");
 var buttonLogin = document.getElementById("buttonLogin");
@@ -104,7 +105,7 @@ var config = {
     authDomain: "pitputit-6af13.firebaseapp.com",
     databaseURL: "https://pitputit-6af13.firebaseio.com",
     projectId: "pitputit-6af13",
-    storageBucket: "",
+    storageBucket: "pitputit-6af13.appspot.com",
     messagingSenderId: "849608190982"
 };
 firebase.initializeApp(config);
@@ -113,33 +114,22 @@ var messagesRef = firebase.database().ref('messages');
 
 // Load existing messages and listen for new ones
 var loadMessages = function() {
-    messagesRef.once('value', function(snap) {
-        var messagesObj = snap.val();
-        messagesArray = flatten(messagesObj);
-        displayMessages();
-    });
-
     messagesRef.on('child_added', function(snap) {
+        console.log('child_added called with ' + (snap.val()));
         insertMessageHtml(snap.val()); // show the new message in the chat box
     })
 };
   
-function sendNewMessage(newMessage) {
+function sendNewMessage(newMessage, onFinished) {        
     messagesRef.push(newMessage, function onComplete(err) {
         if (err) {
-            console.error("sendNewMessage: push failed with " + err);
+            alert("sendNewMessage: push failed with " + err);
+            onFinished(false);
         } else {
             console.log("sendNewMessage: done");
+            onFinished(true);
         }
     });
-}
-
-var flatten = function(obj) {
-    var arr = [];
-    for (prop in obj) {
-        arr.push(obj[prop]);
-    }
-    return arr;
 }
 
 //
